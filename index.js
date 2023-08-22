1341de3f3e78b61ff1e998f9547640906a256764
Anderson <loonander@gmail.com>
2023-08-22 12:49:24 -0300

    mudanças no código e responsividade

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-const botaoImc = () =>{
-    location.href = "imc.html"
+//troca de tela
+const botaoImc = () => {
+    location.href = "reajuste.html"
 }
-const botaoOpe = () =>{
+const botaoOpe = () => {
     location.href = "operation.html"
 }
-const resetBtn = () =>{
+const resetBtn = () => {
     location.href = "index.html"
 }
 
@@ -17,97 +17,97 @@ let result1 = ""
 let result2 = ""
 let operador = ""
 
-let operaçao = (numbers) =>{
-    result.value +=  numbers
-    
-    
+let operaçao = (numbers) => {
+    result.value += numbers
+
+
 }
 
-let calcular = () =>{
-   result2 = Number(result.value)
-   document.getElementsByName('display')[0].placeholder = 0
-   if( result2 !="" && operador == '+'){
-       result.value = (result1 + result2) 
-       result2 = 0
-       operador = ''
-   }else if(result2 !="" && operador == '*'){
-    result.value = (result1 * result2) 
-    result2 = 0
-    operador = ''
-   }else if(result2 !="" && operador == '-'){
-    result.value = (result1 - result2) 
-    result2 = 0
-    operador = ''
-   }else if(result2 !="" && operador == '/'){
-    result.value = (result1 / result2) 
-    result2 = 0
-    operador = ''
-   } 
-    
+let calcular = () => {
+    result2 = Number(result.value)
+    document.getElementsByName('display')[0].placeholder = 0
+    if (result2 != "" && operador == '+') {
+        result.value = (result1 + result2)
+        result2 = 0
+        operador = ''
+    } else if (result2 != "" && operador == '*') {
+        result.value = (result1 * result2)
+        result2 = 0
+        operador = ''
+    } else if (result2 != "" && operador == '-') {
+        result.value = (result1 - result2)
+        result2 = 0
+        operador = ''
+    } else if (result2 != "" && operador == '/') {
+        result.value = (result1 / result2)
+        result2 = 0
+        operador = ''
+    }
+
 }
 
-const operaçoes = (opera) =>{
-    result1 =  Number(result.value)
+const operaçoes = (opera) => {
+    result1 = Number(result.value)
     operador = opera
     result.value = ""
     document.getElementsByName('display')[0].placeholder = result1
-    console.log(result1,operador)
+    console.log(result1, operador)
 }
 
-const deletar = () =>{
-    result.value = result.value.slice(0,-1)
+const deletar = () => {
+    result.value = result.value.slice(0, -1)
     console.log(result1)
 }
-const resetar = () =>{
+const resetar = () => {
     document.getElementsByName('display')[0].placeholder = 0
     result.value = ""
     result1 = ""
 }
 
-// js da calculadora de imc
+// js da calculadora de reajuste
 
 
 const tabela = document.getElementById('table1')
 let resultado = ""
-let valorMudança =  0
+let valorMudança = 0
 
-const calcularReajuste =() =>{
+const calcularReajuste = () => {
     let salario = document.getElementById('salario').value
     let reajuste = document.getElementById('reajuste').value
-    
+
     salario = Number(salario)
     reajuste = Number(reajuste)
     resultado = Number(reajuste / 100 * salario) + salario
-    valorMudança = Number( salario - resultado )
+    valorMudança = Number(salario - resultado)
     valorMudança = Math.abs(valorMudança).toFixed(2)
     resultado = Number(resultado).toFixed(2)
-    console.log(resultado,salario,reajuste,valorMudança)
+   
 
 }
-const exibeResultado =() =>{
- 
-    if(salario.value != ""){
+const exibeResultado = () => {
+
+    if (salario.value != "") {
         calcularReajuste()
         const salarioAtual = document.getElementById('salarioAtual')
-        salarioAtual.value = "salario atual: " + salario.value
+        salarioAtual.value = "Salário atual: " + salario.value
 
         const salarioPorcentagemAu = document.getElementById('salarioPorcentagemAu')
-        salarioPorcentagemAu.value= "porcentagem de reajuste: " + reajuste.value + "%"
-        
+        salarioPorcentagemAu.value = "Porcentagem do aumento: " + reajuste.value + "%"
+
         const salarioMudança = document.getElementById('salarioMudança')
-        salarioMudança.value = "valor do reajuste: " + valorMudança
-      
+        salarioMudança.value = "Valor do reajuste: " + valorMudança
+
         const salarioNovo = document.getElementById('salarioNovo')
-        salarioNovo.value= "salario reajustado: " + resultado
-       
+        salarioNovo.value = "Salário reajustado: " + resultado
+
         document.getElementById("resultados-id").style.display = "block"
         salario.value = ""
         reajuste.value = ""
-        
-    }else{
+
+    } else {
         alert("digite um valor válido")
     }
-    
-    
+
+
 
 }
